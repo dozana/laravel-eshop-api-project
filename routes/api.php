c02080358e226edd306929a96b44a899b34372e9
Levan <levan.soroznishvili@gmail.com>
2023-11-30 22:18:48 +0400

    ui bootstrap auth

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::get('categories', [CategoryController::class, 'index']);
-//Route::get('categories/{category}', [CategoryController::class, 'show']);
-//Route::post('categories', [CategoryController::class, 'store']);
-//Route::put('categories/{category}', [CategoryController::class, 'update']);
-//Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+//Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('categories/{category}', [CategoryController::class, 'show']);
+    Route::post('categories', [CategoryController::class, 'store']);
+    Route::put('categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('categories/{category}', [CategoryController::class, 'destroy']);
+    Route::get('categories/{category_id}/products', [ProductController::class, 'getProductsByCategory']);
 
-Route::apiResource('categories', CategoryController::class);
-
-Route::get('products', [ProductController::class, 'index']);
-Route::get('categories/{category_id}/products', [ProductController::class, 'getProductsByCategory']);
+//    Route::apiResource('categories', CategoryController::class);
+    Route::get('products', [ProductController::class, 'index']);
+//});
